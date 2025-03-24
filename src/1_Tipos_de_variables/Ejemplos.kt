b7af2f7d4322cf3bfbae56928fa395912d22cb6c
BrithanMP <brithanmurillo15@gmail.com>
2025-03-24 10:19:05 -0600

    tipos de variables (me falta)

---
+++ src/1_Tipos_de_variables/Ejemplos.kt
@@ -0,0 +1,21 @@
+package `1_Tipos_de_variables`
+
+fun main() {
+    // Variable inmutable de tipo entero
+    val edad: Int
+    edad = 16
+    //Variable inmutable de tipo decimal
+    val sueldo: Float
+    sueldo = 1200.55f //siempre lleva una "F" al final del valor
+    //Variable inmutable de tipo decimal
+    val total: Double
+    total = 7000.24
+    //variable inmutable de tipo alfanumerica
+    val titulo: String
+    titulo = "sistema de ventas"
+    // variable de tipo mutable, las variables mutables empiezan con var
+    var mes: Int
+    mes = 1
+    //algunas lineas más de codigo
+    mes = 2 
+}
