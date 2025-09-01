b67a3c1573c3d3a529777baced4b72ad8c254e0d
BrithanMP <brithanmurillo15@gmail.com>
2025-09-01 10:43:36 -0600

    Data Class 27

--- src/27_POO_DAta_class/Problema 1.kt
+++ src/27_POO_DAta_class/Problema 1.kt
@@ -2,18 +2,55 @@ package `27_POO_DAta_class`
 
 import kotlin.random.Random
 
-data class butaca(var numero: Int, var ocupado: Boolean)
+data class Butaca(var numero: Int, var ocupada: Boolean) {
+    override fun toString(): String {
+        val estado = if (ocupada) "(ocupada)" else "(libre)"
+        return "Butaca $numero $estado"
+    }
+} 
 
 
-fun main(){
-    val butaca1 = butaca(1, false)
-    val butaca2 = butaca(2, false)
-    val butaca3 = butaca(3, false)
-    val butaca4 = butaca(4, false)
-    val butaca5 = butaca(5, false)
+fun main() {
+    
+    val butacas = mutableListOf(
+        Butaca(1, false),
+        Butaca(2, false),
+        Butaca(3, false),
+        Butaca(4, false),
+        Butaca(5, false)
+    )
 
-      = ((Random.nextBoolean(butaca1,butaca5)))
+
+    var copiaAntesVenta = mutableListOf<Butaca>()
+    for (i in butacas) {
+        copiaAntesVenta += Butaca(i.numero, i.ocupada)
+    }
 
 
+    var vendida1 = Random.nextInt(1, 6)
+    var vendida2 = Random.nextInt(1, 6)
+    while (vendida2 == vendida1) {
+        vendida2 = Random.nextInt(1, 6)
+    }
 
+
+    for (butaca in butacas) {
+        if (butaca.numero == vendida1 || butaca.numero == vendida2) {
+            butaca.ocupada = true
+        }
+    }
+
+    println("Utilizando el toString():")
+    println(butacas)
+
+    println("el indi" + "vidual:")
+    for ((numero, ocupada) in butacas) {
+        val estado = if (ocupada) "ocupada" else "libre"
+        println("Butaca $numero → $estado")
+    }
+
+    println("\nComparación de listas:")
+    println("Contenido igual: ${butacas == copiaAntesVenta}")
+    println("Misma referencia: ${butacas == copiaAntesVenta}")
+    
 }
